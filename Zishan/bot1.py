ae036b0f412772c4e5ad025da968fe644d2fdc2e
ZishanHuang <zishan0huang@gmail.com>
2017-04-07 22:18:19 -0400

    update bot1.py

--- Zishan/bot1.py
+++ Zishan/bot1.py
@@ -2,36 +2,57 @@
 from ws4py.client.threadedclient import WebSocketClient
 import json
 
-url = "ws://127.0.0.1:8080/wsplay"
-key = ""
+url = "ws://localhost:8080/wsplay"
+key = "LouisasCorrectlyScaredRamen"
 
 class WSBot(WebSocketClient):
     frame = 0
     playerNum = 0
+    myself = ""
+    rival = ""
+
     
     def opened(self):
+        # print "opened"
         self.send(key)
 
     def closed(self, code, reason=None):
         print "Closed down", code, reason
 
+    def init(self, data):
+        self.playerNum = data['Player']
+        if self.playerNum == 1:
+            self.myself = "p1"
+            self.rival = "p2"
+        else:
+            self.myself = "p2"
+            self.rival = "p1"
+        # self.myself = (self.playerNum == 1) ? "p1" : "p2"
+        # self.rival = (self.playerNum == 2) ? "p1" : "p2"
+        print "Username:", data["Username"], " Player: ", data["Player"], " Gamename: ", data["Gamename"]
+
+    def terminates(self, data):
+        if data[self.myself]["mainCore"]["hp"] <= 0:
+            print "User lost :("
+            return True
+        elif data[self.rival]["mainCore"]["hp"] <= 0:
+            print "User won :)"
+            return True
+        return False
+
     def received_message(self, m):
         data = json.loads(m.data)
         print data
         if self.frame == 0: # expect status message
-            print "Username:", data["Username"], " Player: ", data["Player"], " Gamename: ", data["Gamename"]
-            self.playerNum = data["Player"]
+            self.init(data)
             self.frame += 1
             return
-        if data["p1"]["mainCore"]["hp"] <= 0 or data["p2"]["mainCore"]["hp"] <= 0:
-            if (data["p1"]["mainCore"]["hp"] <= 0 and self.playerNum == 1) or (data["p2"]["mainCore"]["hp"] <= 0 and self.playerNum == 2):
-                print "User lost :("
-            else:
-                print "User won :)"
+        if self.terminates(data):
             ws.close()
         self.frame += 1
-        if data['p' + str(self.playerNum)]["bits"] >= 10000:
-            self.send("b00 01")
+        if data[self.myself]["bits"] >= 10000:
+            for i in range(50):
+                self.send("b00 03")
 
 if __name__ == '__main__':
     try:
